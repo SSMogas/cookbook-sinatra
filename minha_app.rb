489d5d3638591079e53d1a9cc81627750426fcd2
Sofia Mogas <sofia.mogas@gmail.com>
2019-11-24 19:38:19 +0000

    Boilerplate sinatra app

---
+++ minha_app.rb
@@ -0,0 +1,12 @@
+require "sinatra"
+require "sinatra/reloader" if development?
+require "pry-byebug"
+require "better_errors"
+configure :development do
+  use BetterErrors::Middleware
+  BetterErrors.application_root = File.expand_path('..', __FILE__)
+end
+
+get '/' do
+  'O meu primeiro web service e o meu primeiro https request!'
+end
